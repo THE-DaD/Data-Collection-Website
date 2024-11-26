fd85bfbad5d573ada341d14069c07978592c6a72
Innon <innonrivk@gmail.com>
2024-11-26 20:30:22 +0200

    26.11 commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    distDir: '.next', // This tells Next.js to build to the '.next' directory
+    // ... other configuration options
+  };
+  
+  // Export using ES module syntax
+  export default nextConfig;
+  
