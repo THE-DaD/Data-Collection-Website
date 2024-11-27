e6bec39d9f17d55ffa429512dcdb6a83b769a5e4
Innon <innonrivk@gmail.com>
2024-11-27 11:01:56 +0200

    hosting works!!!

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+const nextConfig = {
+  distDir: 'out', // This specifies that the exported static files should go into the 'out' directory
+  output: 'export', // Ensures Next.js will export the app as static HTML
+};
+
+module.exports = nextConfig;
--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    distDir: '.next', // This tells Next.js to build to the '.next' directory
-    // ... other configuration options
-  };
-  
-  // Export using ES module syntax
-  export default nextConfig;
-  
