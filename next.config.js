563c2f28992c5dd63c11fb874913034d1f744570
Innon Rivkin <innonrivk@gmail.com>
2024-12-02 18:49:43 +0200

    fixed deploy problem.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,1 @@
-// next.config.js
-const nextConfig = {
-  distDir: 'out', // This specifies that the exported static files should go into the 'out' directory
-  output: 'export', // Ensures Next.js will export the app as static HTML
-};
-
-module.exports = {
-  output: 'standalone', // Configure output mode for static export
-};
+module.exports = {};
