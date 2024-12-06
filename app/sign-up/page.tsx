21aa74b8be7c3fb9d98a841f79e6debdc5ffda8b
Innon Rivkin <innonrivk@gmail.com>
2024-12-06 21:22:41 +0200

    backbtn implemented

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,24 +1,54 @@
 'use client'
 
-
 import { Button } from '@/components/ui/button'
-
 import firebaseapi from '../../firebase/firebaseapi';
+import './signup.css';
+import { ArrowRight} from 'lucide-react';
+import Link from 'next/link'
+
+
 
 export default function SignUp() {
     
-    const  handleClick = async () => {
-     await firebaseapi.signInWithGoogle();
-   }
+  const  handleClick = async () => {
+   await firebaseapi.signInWithGoogle();
+ }
 
-
+   
 
   
   return (
-    <div>
-        <Button onClick={handleClick}>
-            Sign With Google
-        </Button>
+    <div style={{width: "100%", height: "100%", overflow: "hidden"}}>
+      <div style={{position: "absolute", top: "0%", left: "1%"}}>
+      <header className="">
+          <div className="">
+            <Link 
+              href="/" 
+            >
+              חזרה לדף הבית
+              <ArrowRight className="w-4 h-4 mr-2" />
+            </Link>
+          </div>
+        </header>
+      </div>
+    <div className="signup-page">
+      <div className="background-text">
+      <p> מצוינות <br/>
+      &emsp;&emsp;הקשבה<br/>
+      התקדמות<br/>
+      &emsp;&emsp;סובלנות<br/>
+      מנהיגות</p>
+
+      </div>
+
+      <h1>הכל<br/>מתחיל<br/>פה</h1>
+
+      <div className="arrow"></div>
+
+      <Button className="button" onClick={handleClick}>
+       Sign With Google
+      </Button>
+    </div>
     </div>
   )
 }
